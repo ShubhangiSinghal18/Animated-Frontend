ffbde504abe9a9da5bf1c77cb92ce893b46e9ccd
ShubhangiSinghal18 <shubhangisinghal550@gmail.com>
2025-04-26 19:04:40 +0530

    commit 2

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const App = () => {
   return (
     <div>
-      <h1 className='text-5xl text-blue-50 font-bold'>Hellow!</h1>
+      <h1 className='text-5xl text-blue-50 font-bold'>Hello World!</h1>
     </div>
   )
 }
