ad235d24084fe45f74a45c468518c22c16840d04
ShubhangiSinghal18 <shubhangisinghal550@gmail.com>
2025-04-26 19:00:42 +0530

    Tailwind v4.1 Setup

---
+++ README.md
@@ -0,0 +1,1 @@
+- Tailwind V4.1 installation 
---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div>
+      <h1 className='text-5xl text-blue-50 font-bold'>Hellow!</h1>
+    </div>
+  )
+}
+
+export default App
