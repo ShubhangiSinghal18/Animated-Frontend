a54a0359401c2d0198dd62b00745cdbcee371b32
ShubhangiSinghal18 <shubhangisinghal550@gmail.com>
2025-05-14 17:29:19 +0530

    NavBar Enhancement

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,11 +2,13 @@ import React, { useEffect, useRef, useState } from "react";
 import Button from "./Button";
 import { TiLocationArrow } from "react-icons/ti";
 import "../index.css";
-import {useWindowScroll} from 'react-use'
-import gsap from 'gsap';
+import { useWindowScroll } from "react-use";
+import gsap from "gsap";
+import { TbMusicPause } from "react-icons/tb";
 const navItems = ["Home", "About", "Vault", "Prologue", "Contact"];
 
 const Navbar = () => {
+  const ismdScreen = window.innerWidth >= 768;
   const navContainerRef = useRef(null);
   const audioElementRef = useRef(null);
   const [isAudioPlaying, setisAudioPlaying] = useState(false);
@@ -25,35 +27,35 @@ const Navbar = () => {
     }
   }, [isAudioPlaying]);
 
-  const {y: currentScrollY} = useWindowScroll();
-  const [lastScrollY, setlastScrollY] = useState(0)
-  const [isNavVisible, setisNavVisible] = useState(true)
-    useEffect(()=>{
-      if(currentScrollY===0){
-        setisNavVisible(true);
-        navContainerRef.current.classList.remove('floating-nav');
-      }
-     else if(currentScrollY > lastScrollY){
-        setisNavVisible(false);
-        navContainerRef.current.classList.add('floating-nav');
-      }
-      else if (currentScrollY<lastScrollY){
-        setisNavVisible(true);
-        navContainerRef.current.classList.add('floating-nav');
-      }
+  const { y: currentScrollY } = useWindowScroll();
+  const [lastScrollY, setlastScrollY] = useState(0);
+  const [isNavVisible, setisNavVisible] = useState(true);
 
-      setlastScrollY(currentScrollY);
-    } , [currentScrollY ,lastScrollY])
+  useEffect(() => {
+   // console.log(ismdScreen)
+    if (currentScrollY === 0) {
+      setisNavVisible(true);
+      navContainerRef.current.classList.remove("floating-nav");
+    } else if (currentScrollY > lastScrollY) {
+      setisNavVisible(false);
+      navContainerRef.current.classList.add("floating-nav");
+    } else if ((currentScrollY < lastScrollY) && ismdScreen) {
+      setisNavVisible(true);
+      navContainerRef.current.classList.add("floating-nav");
+    }
 
-    useEffect(()=>{
-gsap.to(navContainerRef.current , {
-  y: isNavVisible? 0: -100,
-  opacity: isNavVisible? 1:0,
-  duration:0.2,
+    setlastScrollY(currentScrollY);
+  }, [currentScrollY, lastScrollY]);
 
-})
-
-    } ,[isNavVisible])
+  useEffect(() => {
+  
+    gsap.to(navContainerRef.current, {
+      y: isNavVisible ? 0 : -100,
+      opacity: isNavVisible ? 1 : 0,
+      duration: 0.2,
+    });
+  
+  }, [isNavVisible]);
 
   return (
     <div
@@ -61,7 +63,7 @@ gsap.to(navContainerRef.current , {
       className="fixed inset-x-0 top-4 z-50 h-16 border-none transition-all duration-700 sm:inset-x-6"
     >
       <header className="absolute top-1/2 w-full -translate-y-1/2 ">
-        <nav className="flex size-full items-center justify-between p-4">
+        <nav className="flex size-full items-center justify-between p-4 ">
           <div className="flex items-center gap-7">
             <img src="/img/logo.png" alt="logo" className="w-10" />
 
@@ -73,14 +75,13 @@ gsap.to(navContainerRef.current , {
             />
           </div>
 
-          <div className="flex h-full items-center ">
+          <div className="flex h-full items-center">
             <div className="hidden md:block ">
-            
               {navItems.map((item, index) => (
                 <a
                   key={index}
                   className="nav-hover-btn"
-                  href={`#${item.toLocaleLowerCase()}`}
+                  href={`#${item.toLowerCase()}`}
                 >
                   {item}
                 </a>
@@ -91,21 +92,24 @@ gsap.to(navContainerRef.current , {
               className="ml-10 flex items-center space-x-0.5"
               onClick={toggleAudioIndicator}
             >
+             
               <audio
                 ref={audioElementRef}
                 className="hidden"
                 src="/audio/loop.mp3"
                 loop
               />
-              {[1, 2, 3, 4].map((bar) => (
-                <div
-                  key={bar}
-                  className={`cursor-pointer indicator-line ${
-                    isIndicatorActive ? "active" : ""
-                  }`}
-                  style={{ animationDelay: `${bar * 0.1}s` }}
-                />
-              ))}
+              {
+                isAudioPlaying? [1, 2, 3, 4].map((bar) => (
+                  <div
+                    key={bar}
+                    className={`cursor-pointer indicator-line ${
+                      isIndicatorActive ? "active" : ""
+                    }`}
+                    style={{ animationDelay: `${bar * 0.1}s` }}
+                  />
+                )): <TbMusicPause color="white" size={20} className="cursor-pointer"/>
+              }
             </button>
           </div>
         </nav>
@@ -115,3 +119,5 @@ gsap.to(navContainerRef.current , {
 };
 
 export default Navbar;
+
+ 
